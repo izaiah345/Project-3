65269ac4b00f2508f0cb8be4d538129b4467fd59
izaiah365 <izaiah.rodriguez@patelhs.org>
2020-11-04 17:34:00 +0000

    in putting scanner

--- Main.java
+++ Main.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 class Main {
   public static void main(String[] args) {
     int [] seats = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
@@ -6,9 +7,12 @@ class Main {
 
       for (int i = 0; i < names.length; i++)
       {
-        System.out.println(names [i]);
+        System.out.println("Type your name here ");
+        names [i] = scan.next(); 
       }
-       
+      
+     
+
 
 
 
