abb0b4794055f1884ce628d56d3ec5fe87ec8740
izaiah365 <izaiah.rodriguez@patelhs.org>
2020-11-10 13:48:01 +0000

    added comments

--- Main.java
+++ Main.java
@@ -2,16 +2,16 @@ import java.util.Scanner;
 class Main {
   public static void main(String[] args) {
     int [] seats = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
-
+     //this starts my scanner 
      String[] names = new String[15];
       Scanner scan = new Scanner(System.in);
-
+       
       for (int i = 0; i < names.length; i++)
       {
         System.out.println("Type your name here ");
         names [i] = scan.next();
       }
-      
+     //this starts my loop 
      int count = 0;
      while (count <= 14)
      {
