e286c696ba17509938e5eec71401bf9674db74b1
izaiah365 <izaiah.rodriguez@patelhs.org>
2020-11-16 18:33:28 +0000

    Added more comments to my code

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 class Main {
   public static void main(String[] args) {
+    // this is how may children there going to be 
     int [] seats = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
      //this starts my scanner 
      String[] names = new String[15];
@@ -15,6 +16,7 @@ class Main {
      int count = 0;
      while (count <= 14)
      {
+       //this starts and ends my loop 
        System.out.println(names [count] + " sits in seat " + seats [count]);
         count++;
      }
